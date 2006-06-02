c63ca6dacc76c7b24065bfc382f35cde52e258b5
Mats Bengtsson <matben@privat.utfors.se>
2006-06-02 12:24:26 +0000

    * Added image.tcl demo file.

---
+++ demos/image.tcl
@@ -0,0 +1,17 @@
+package require tkpath
+
+set t .c_image
+toplevel $t
+set w $t.c
+pack [canvas $w -width 400 -height 400 -bg white]
+
+set ::tkpath::antialias 1
+
+set f /Users/matben/Tcl/cvs/coccinella/images/coccinella32.gif
+set name [image create photo -file $f]
+$w create pimage 100 100 -image $name
+
+set id [$w create prect 100 100  \
+  [expr 100+[image width $name]] [expr 100+[image height $name]]]
+$w move $id 0.5 0.5
+
