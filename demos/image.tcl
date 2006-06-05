9b73fb9bc3ea56775468e81fa0f52c4fedf88cd2
Mats Bengtsson <matben@privat.utfors.se>
2006-06-05 14:48:21 +0000

    * Added test image.  * image.tcl modified.

--- demos/image.tcl
+++ demos/image.tcl
@@ -11,10 +11,25 @@ pack [canvas $w -width 400 -height 400 -bg white]
 
 set ::tkpath::antialias 1
 
+set dir [file dirname [info script]]
+set f [file join $dir trees.gif]
 set name [image create photo -file $f]
-$w create pimage 100 100 -image $name
+set x 20
+set y 20
+$w create pimage $x $y -image $name
 
-set id [$w create prect 100 100  \
-  [expr 100+[image width $name]] [expr 100+[image height $name]]]
+set id [$w create prect $x $y  \
+  [expr $x+[image width $name]] [expr $y+[image height $name]]]
 $w move $id 0.5 0.5
+
+set m [::tkpath::transform rotate 0.5]
+lset m {2 0} 220
+lset m {2 1} -120
+$w create pimage 100 100 -image $name -matrix $m
+
+set m [::tkpath::transform scale 3 1]
+$w create pimage 10 220 -image $name -matrix $m
+
+
+
 
