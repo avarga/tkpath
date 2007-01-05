aafea050210833c4c73429be65ea4a1dd8e50b80
Mats Bengtsson <matben@privat.utfors.se>
2007-01-05 14:57:33 +0000

    * Added a text item to demo.

--- demos/text.tcl
+++ demos/text.tcl
@@ -7,6 +7,8 @@ pack [canvas $w -width 400 -height 400 -bg "#c6ceef" -highlightthickness 0]
 
 set ::tkpath::antialias 1
 
+$w create ptext 100 360 -text X -fontsize 400 -fill "" \
+  -stroke gray -strokewidth 2
 $w create ptext 0 0 -text Coccinella -fontfamily Helvetica -fontsize 64 \
   -fill white -fillopacity 0.7 -matrix {{1 0.3} {-0.3 1} {50 80}} \
   -stroke gray -strokewidth 2
