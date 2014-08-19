dcdaf9c03093dc7602878662a9d5e47949b59e08
Andras Varga <andras@omnetpp.org>
2014-08-19 16:32:33 +0200

    demos: some files contained old Mac line endings (CR only)

--- demos/all.tcl
+++ demos/all.tcl
@@ -1,1 +1,9 @@
-set dir [file dirname [info script]]set tail [file tail [info script]]foreach fileName [glob -nocomplain -directory $dir *.tcl] {    if {[file tail $fileName] ne $tail} {	source $fileName    }}
+
+set dir [file dirname [info script]]
+set tail [file tail [info script]]
+foreach fileName [glob -nocomplain -directory $dir *.tcl] {
+    if {[file tail $fileName] ne $tail} {
+	source $fileName
+    }
+}
+
